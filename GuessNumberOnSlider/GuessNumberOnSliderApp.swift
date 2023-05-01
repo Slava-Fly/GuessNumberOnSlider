11522d1385de3d665685ddd711e695aedf7b00bd
@Slava-Fly <school.94@bk.ru>
2023-05-01 17:06:58 +0800

    Initial Commit

---
+++ GuessNumberOnSlider/GuessNumberOnSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberOnSliderApp.swift
+//  GuessNumberOnSlider
+//
+//  Created by User on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberOnSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
